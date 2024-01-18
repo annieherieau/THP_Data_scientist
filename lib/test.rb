eebbbca0220e79be81014e0281d37031cf7451bf
Annie Hérieau <148761809+annieherieau@users.noreply.github.com>
2024-01-18 21:12:29 +0100

    Delete lib/test.rb

--- lib/test.rb
+++
@@ -1,8 +0,0 @@
-require_relative "./arrays.rb"
-
-monarray = ["@jcunnyiet","@PaulLampon","@Azilyiz31","@ssouMier","@marionsouzeau","@gaeyllombart"]
-
-
-puts monarray.count{ |handle|  /[[:upper:]]/.match(handle[1]) }
-
-
